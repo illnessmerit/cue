55173a7fb7d77c75354c465017c95815d9031f33
a <a@as-Virtual-Machine.local>
2026-02-03 22:27:51 -0800

    Initialize Clojure development environment

--- devenv.nix
+++ devenv.nix
@@ -20,6 +20,7 @@
 
   # https://devenv.sh/languages/
   # languages.rust.enable = true;
+  languages.clojure.enable = true;
 
   # https://devenv.sh/processes/
   # processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
