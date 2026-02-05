0fe84b9677fefea7de43261ee05d2084d8fd530b
a <a@as-Virtual-Machine.local>
2026-02-04 19:47:44 -0800

    Update clj-kondo configuration and initialize Qwen tokenizer

--- src/core.clj
+++ src/core.clj
@@ -1,7 +1,10 @@
 (ns core
   (:require
-   [libpython-clj2.python :refer [from-import initialize!]]))
+   [libpython-clj2.python :refer [$a from-import initialize!]]))
 
 (initialize!)
 
 (from-import transformers AutoTokenizer)
+
+(def tokenizer
+  ($a AutoTokenizer from-pretrained "Qwen/Qwen3-0.6B-Base"))
